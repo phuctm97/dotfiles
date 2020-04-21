0ae7c80d1288a0e793cc4e731efc09efbc2a948f
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-21 08:09:43 +0700

    feat: improve install.sh and main.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-# Install XCode Command Line Tools and wait til it's finished.
-# XCode Command Line Tools are required for all next steps including Git clone, executing Python
-# scripts, installing Homebrew, etc.
-xcode-select --install &> /dev/null
-until $(xcode-select --print-path &> /dev/null); do
-  sleep 5;
-done
-
 # Where to install dotfiles (clone dotfiles repository) from and to.
-readonly DOTFILES_REPO="https://github.com/phuctm97/dotfiles.git"
-readonly DOTFILES_DIR="$HOME/.dotfiles"
+declare -r DOTFILES_REPO="https://github.com/phuctm97/dotfiles.git"
+declare -r DOTFILES_DIR="$HOME/.dotfiles"
 
-# Clone dotfiles repository.
-mkdir -p $DOTFILES_DIR
-cd $DOTFILES_DIR
-git clone $DOTFILES_REPO .
+are_xcode_command_line_tools_installed() {
+  xcode-select --print-path &> /dev/null
+}
 
-# Source main.sh.
-. src/main.sh
+install_xcode_command_line_tools() {
+  xcode-select --install &> /dev/null
+}
+
+install() {
+  if ! are_xcode_command_line_tools_installed; then
+    # Install XCode Command Line Tools and wait til it's finished.
+    # XCode Command Line Tools are required for all next steps including Git clone, executing
+    # Python scripts, installing Homebrew, etc.
+    install_xcode_command_line_tools
+    until are_xcode_command_line_tools_installed; do
+      sleep 5;
+    done
+  fi
+
+  # Clone dotfiles repository.
+  mkdir -p $DOTFILES_DIR
+  cd $DOTFILES_DIR
+  git clone $DOTFILES_REPO .
+
+  # Source main.sh.
+  . src/main.sh
+}
+
+install "$@"
--- src/main.sh
+++ src/main.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-readonly SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-readonly CFG="$(cd "$(dirname "$SRC")/cfg" >/dev/null 2>&1 && pwd)"
+declare -r SRC="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+declare -r CFG="$(cd "$(dirname "$SRC")/cfg" >/dev/null 2>&1 && pwd)"
 
-. $SRC/enable-sudo-auth-touch-id.sh
+main() {
+  . $SRC/enable-sudo-auth-touch-id.sh
 
-. $SRC/install-homebrew.sh
+  . $SRC/install-homebrew.sh
 
-. $SRC/fix-zsh-permissions.sh
+  . $SRC/fix-zsh-permissions.sh
 
-. $SRC/install-antigen.sh
+  . $SRC/install-antigen.sh
 
-. $SRC/install-fonts.sh
+  . $SRC/install-fonts.sh
 
-. $SRC/install-tools.sh
+  . $SRC/install-tools.sh
 
-. $SRC/create-syslinks.sh
+  . $SRC/create-syslinks.sh
+}
+
+main "$@"
