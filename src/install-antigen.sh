2d0d33b8d4d606e3fab1257cb51efc9a3aab8ad8
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-21 09:23:35 +0700

    fix: improve install-antigen

--- src/install-antigen.sh
+++ src/install-antigen.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Install Antigen.
-CWD=$(pwd)
-cd /usr/local/share
-mkdir antigen
-sudo chmod -R 755 antigen
-cd antigen
-curl -L git.io/antigen > antigen.zsh
-cd $CWD
+main() {
+  local cwd="$(pwd)"
+
+  # Install Antigen.
+  cd /usr/local/share
+  mkdir antigen
+  sudo chmod -R 755 antigen
+  cd antigen
+  curl -L git.io/antigen > antigen.zsh
+
+  cd $cwd
+}
+
+main "$@"
