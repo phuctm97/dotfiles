ed85c4354030e4d73afe31a3086c80610203084b
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-21 09:09:47 +0700

    fix: only install homebrew when necessary

--- src/install-homebrew.sh
+++ src/install-homebrew.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Install Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+main() {
+  if command -v "brew" &> /dev/null; then
+    return
+  fi
+
+  # Install Homebrew.
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+}
+
+main "$@"
