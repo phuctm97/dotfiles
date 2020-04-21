0424a63d767c7c1804c9d1354d3093def9ff0149
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-21 09:17:16 +0700

    fix: improve fix-zsh-perm

--- src/fix-zsh-permissions.sh
+++ src/fix-zsh-permissions.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Fix permission issues with ZSH.
-CWD=$(pwd)
-cd /usr/local/share
-sudo chmod -R 755 zsh
-cd $CWD
+main() {
+  local cwd="$(pwd)"
+
+  # Fix permission issues with ZSH.
+  cd /usr/local/share
+  sudo chmod -R 755 zsh
+
+  cd $cwd
+}
+
+main "$@"
